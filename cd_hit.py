642240d3fe7308baed2b83a654dd34825f542348
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-07-25 19:37:08 +0300

    Update cd_hit.py

--- cd_hit.py
+++ cd_hit.py
@@ -3,9 +3,17 @@
 
 # In[1]:
 
-
 import sys
 import os
+import apt
+
+cache = apt.Cache()
+if cache['cd-hit'].is_installed:
+    print "YES it's installed"
+else:
+    print "NO it's NOT installed"
+    os.system(f'sudo apt-get install cd-hit')
+ 
 db=sys.argv[1]
 out=sys.argv[2]
 
@@ -19,3 +27,5 @@ def cdhit_meta(db,out):
 
 cdhit_meta(db,out)
 
+if __name__ == "__main__":
+    main()
