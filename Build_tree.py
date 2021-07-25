41387f925a6a8d3d70f6639117e42e633c632d29
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-07-25 19:45:14 +0300

    Update Build_tree.py

--- Build_tree.py
+++ Build_tree.py
@@ -3,6 +3,15 @@ import os
 db=sys.argv[1]
 out=sys.argv[2]
 
+cache = apt.Cache()
+if cache['mafft', 'iqtree'].is_installed:
+    print "YES it's installed"
+else:
+    print "NO it's NOT installed"
+    os.system(f'sudo apt-get update -y')
+    os.system(f'sudo apt-get install -y mafft')
+    os.system(f'sudo apt-get install -y iqtree')
+
 def cdhit_meta(db,out):
     
     
