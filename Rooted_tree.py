1d572b4aa91c62fa4629fb5fb186e8119b43ce1e
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-07-26 14:47:48 +0300

    Update Rooted_tree.py

--- Rooted_tree.py
+++ Rooted_tree.py
@@ -26,4 +26,4 @@ tree <- root(tree, outgroup=1, resolve.root=TRUE)
 tree<- midpoint.root(tree)
 
 group<-groupClade(tree, .node=c(min(edge$parent):max(edge$parent)))
-ggtree(group, aes(color=group), rooted=TRUE)+geom_tiplab(size=2)+theme(legend.position='none')
+ggtree(group, aes(color=group), rooted=TRUE)+geom_tiplab(size=1.5)+theme(legend.position='none')
