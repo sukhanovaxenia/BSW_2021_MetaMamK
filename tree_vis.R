833ebb989662e376212863a0c7af4d8fda873392
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-07-26 17:55:46 +0300

    Update tree_vis.R

--- tree_vis.R
+++ tree_vis.R
@@ -9,19 +9,20 @@ library(treeio)
 library(ggtree)
 library(phytools)
 library(dplyr)
-tree_vis<-function(treefile=dir(pattern = '*fa.treefile'),taxfile=dir(pattern='^parsed.*?\\.txt'), pngfile='tree_plot.png'){
-  tree <- read.tree(treefile)
-  tree_r<- midpoint.root(tree)
-  edge<-data.frame(tree_r$edge, edge_num=1:nrow(tree_r$edge))
-  colnames(edge)<-c('parent', 'node', 'edge_num')
-  taxonomy<-read.table(taxfile, sep='|', header=F)
-  colnames(taxonomy)<-c('Accession', 'Protein', 'Lineage', 'Ecology')
-  taxonomy2<-filter(taxonomy, Accession %in% tree_r$tip.label)
-  tree_r$tip.label<-taxonomy2$Ecology
-  #Load unique parents numbers into group:
-  group<-groupClade(tree_r, .node=c(min(edge$parent):max(edge$parent)))
-  png(filename=pngfile)
-  ggtree(group, aes(color=group), rooted=TRUE)+geom_tiplab(size=1.8)+theme(legend.position='none')
-  dev.off()
-}
-tree_vis(pngfile='tree_plot.png')
+treefile=dir(pattern = '*fa.treefile')
+taxfile=dir(pattern='^parsed.*?\\.txt')
+pngfile='tree_plot.png'
+tree <- read.tree(treefile)
+tree_r<- midpoint.root(tree)
+edge<-data.frame(tree_r$edge, edge_num=1:nrow(tree_r$edge))
+colnames(edge)<-c('parent', 'node', 'edge_num')
+taxonomy<-read.table(taxfile, sep='|', header=F)
+colnames(taxonomy)<-c('Accession', 'Protein', 'Lineage', 'Ecology')
+taxonomy2<-filter(taxonomy, Accession %in% tree_r$tip.label)
+tree_r$tip.label<-taxonomy2$Ecology
+#Load unique parents numbers into group:
+group<-groupClade(tree_r, .node=c(min(edge$parent):max(edge$parent)))
+png(filename=pngfile)
+ggtree(group, aes(color=group), rooted=TRUE)+geom_tiplab(size=1.8)+theme(legend.position='none')
+dev.off()
+
